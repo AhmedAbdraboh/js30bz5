9d91125def7c49c3f15f506486d2303ec48d7709
Ahmed Abdraboh <ahmed.elsayed.abdraboh@gmail.com>
2019-06-06 16:25:40 +0200

    feat: get route (#3) (#6)

--- src/models/buyers.js
+++ src/models/buyers.js
@@ -1,17 +1,34 @@
-var redis = require('../redis')();
-const buyers = "buyers"
+var redis = require('../redis')()
+const buyers = 'buyers'
+const routes = 'routes'
 
 module.exports = {
-    create: set,
-    get: get
+  create: set,
+  get: get,
+  getRoute: getRoute
 }
 
-function set(obj, cb) {
-    let key = buyers + ":" + obj.id;
-    return redis.set(key, JSON.stringify(obj), cb)
+function set (obj, cb) {
+  let key = buyers + ':' + obj.id
+  let cmd = redis.multi()
+    .set(key, JSON.stringify(obj))
+  for (let offer of obj.offers) {
+    for (let state of offer.criteria.state) {
+      for (let device of offer.criteria.device) {
+        let routeSetKey = routes + ':' + state + ':' + device
+        cmd.zadd(routeSetKey, offer.value, offer.location)
+      }
+    }
+  }
+  return cmd.exec(cb)
 }
 
-function get(id, cb) {
-    let key = buyers + ":" + id;
-    return redis.get(key, cb)
+function get (id, cb) {
+  let key = buyers + ':' + id
+  return redis.get(key, cb)
+}
+
+function getRoute (query, cb) {
+  let key = routes + ':' + query.state + ':' + query.device
+  return redis.zrevrange(key, 0, 0, cb)
 }
